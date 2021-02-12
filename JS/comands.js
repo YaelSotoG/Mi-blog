d1e699c9f60f64244134408d20171c315cf12fe5
Yael Soto <xyarock@gmail.com>
2021-02-12 02:16:05 -0600

    se le agrego evento al js

--- JS/comands.js
+++ JS/comands.js
@@ -1,3 +1,7 @@
-window.onload =function(){
-    let intro= document.getElementById("vista");
-}
+// window.onload =function(){
+//     let intro= document.getElementById("vista");
+// }
+
+document.querySelector('#menu').addEventListener("click",()=>{
+    console.log("hola mundo")
+});
